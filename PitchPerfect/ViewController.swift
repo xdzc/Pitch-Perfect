dbb600f930934e82125b298014fc0d9ca9973256
Cinfwat Dogak <cdogakz@yahoo.com>
2017-10-07 10:53:53 +0100

    Add barebones IBOutlets and IBActions for the UI components

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record pressed!")
+        recordingLabel.text = "Recording in progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording pressed!")
+    }
 }
 
